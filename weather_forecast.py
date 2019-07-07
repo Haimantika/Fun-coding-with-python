676c08454800d651c8dfdb9b7bcebf428a84e21a
haimantika mitra <32809211+Haimantika@users.noreply.github.com>
2019-07-07 20:26:27 +0530

    Add files via upload
    This a 5 line code in python which will tell you the weather forecast of the place entered by you.
    
    Use command: pip install selenium
    
    And download the webdriver of the browser you are using for chrome check the version you are using and download using the link: https://sites.google.com/a/chromium.o...
     Do the same for firefox.
    
    Edit: if you are using firefox change the second line of the code as 
    driver = webdriver.Firefox()

---
+++ weather_forecast.py
@@ -0,0 +1,17 @@
+from selenium import  webdriver
+
+#if using Chrome
+driver = webdriver.Chrome()
+
+#if using Firefox
+# driver = webdriver.Firefox()
+
+
+# Replace space from - For example new-york.
+city = str(input("Enter the name of the city you want the weather forecast for: ")).replace(" ","-")
+
+try:
+    driver.get("https://www.weather-forecast.com/locations/"+city+"/forecasts/latest")
+    print(driver.find_elements_by_class_name("b-forecast__table-description-content")[0].text)
+except:
+    print("Something went wrong")
